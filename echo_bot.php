d26cf7c50d3fa9c6b835da5472c5fedb42599cff
cnuhao <38758331+cnuhao@users.noreply.github.com>
2018-05-12 21:30:04 +0800

    Add files via upload

---
+++ echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'sXpXtvMAPlqVYu1YfXHtLdzSK7Htz3QrYfYEcGgxGbbe40JiwH0/qI5ymEmgJGjVWnK5YNMcajf6hIJVnmE6dN/ggw9pIL7QS+azlJvgH6/jiq9texZe+UZq30OZgAby4E2oY1xrKsFtD4otzZKEawdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '32507a7780b3898758bbcc9efb52a739';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
